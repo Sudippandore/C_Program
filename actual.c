85c018b9990af2d606e01f785c1c8f8d121c15e1
Sudip Pandore <sudippandore9890@gmail.com>
2026-06-23 21:01:59 +0530

    c

---
+++ actual.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+	int num=6;
+int result = factorail(num);
+printf("factorail of %d=%d",&num,&result);
+	
+	
+	
+	
+	
+	
+	
+
+int n;
+int factorail(int  n);
+{
+	int i,fact=i;
+	for(i=1;i<=n;i++)
+
+{
+	fact=fact*i;
+}
+return fact;
+}
+}
